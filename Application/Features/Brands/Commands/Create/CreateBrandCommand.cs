9a648e0b32eb51afce6db309a4605832fb36e4b2
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-03-01 09:39:39 +0300

    Add project files.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand:IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse response = new(); //Cqrs anlamaya çalıştığım için hata almamak kaydıyla çalıştığını görmek için yazıldı bu kısım
+            response.Name = request.Name;
+            response.Id = new Guid();
+            return null;
+        }
+    }
+}
---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class BaseController:ControllerBase
+{
+    private IMediator? _mediator;
+    protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>(); 
+    // Eğer elinde mediator varsa onu döndür eğer yoksa bunu set et.
+
+}
