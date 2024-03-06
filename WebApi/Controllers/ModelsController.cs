8ede90e8437465fa54286eef11b5be5721ebf904
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-03-06 12:10:20 +0300

    Dynamic Altyapımızın İmplementasyonu

--- WebApi/Controllers/ModelsController.cs
+++ WebApi/Controllers/ModelsController.cs
@@ -1,7 +1,9 @@
 ﻿using Application.Features.Brands.Queries.GetList;
 using Application.Features.Models.Queries.GetList;
+using Application.Features.Models.Queries.GetListByDynamic;
 using Core.Application.Requests;
 using Core.Application.Responses;
+using Core.Persistence.Dynamic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +18,13 @@ namespace WebApi.Controllers
         {
             GetListModelQuery getListModelQuery = new() { PageRequest = pageRequest };
             GetListResponse<GetListModelListItemDto> response = await Mediator.Send(getListModelQuery);
+            return Ok(response);
+        }
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<IActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] DynamicQuery? dynamicQuery = null)
+        {
+            GetListByDynamicModelQuery getListByDynamicModelQuery = new() { PageRequest = pageRequest, DynamicQuery = dynamicQuery };
+            GetListResponse<GetListByDynamicModelListItemDto> response = await Mediator.Send(getListByDynamicModelQuery);
             return Ok(response);
         }
     }
