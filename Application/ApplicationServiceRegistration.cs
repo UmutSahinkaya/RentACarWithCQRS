8851e3b658eba89de8519e0acff4f2e9239e6def
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-03-07 09:25:33 +0300

    İş Kuralları ve SubClass Type IoC implementasyonu

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Services.Repositories;
+using Core.Application.Rules;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -14,12 +15,23 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
+        services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
         services.AddMediatR(configuration =>
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
         });
-
         return services;
     } 
+
+    //Git Assembly içerisinde benim Subclass(BaseBussiness) olarak verdiğim sınıfları bul ve IOC ye ekle.
+    public static IServiceCollection AddSubClassesOfType(this IServiceCollection services,Assembly assembly,Type type,Func<IServiceCollection,Type,IServiceCollection>? addWithLifeCycle=null)
+    {
+        var types=assembly.GetTypes().Where(t=>t.IsSubclassOf(type) && type != t).ToList();
+        foreach(var t in types)
+            if (addWithLifeCycle == null)
+                services.AddScoped(t);
+            else
+                addWithLifeCycle(services, type);
+        return services;
+    }
 }
