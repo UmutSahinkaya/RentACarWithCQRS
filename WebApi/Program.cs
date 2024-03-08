5c657adf0f5735a22eac3bdc9600cecce1d97318
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-03-08 11:06:57 +0300

    Mediatr Pipeline ile Add Caching İmplementasyonu InMemory Cache İmplementasyonu Cache Altyapısının Docker Üzerinde Redis Ortamına Taşınması

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersitenceServices(builder.Configuration);
+
+//builder.Services.AddDistributedMemoryCache();
+builder.Services.AddStackExchangeRedisCache(opt => opt.Configuration = "localhost:6379");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
